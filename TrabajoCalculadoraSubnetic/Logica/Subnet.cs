2ae45a6e39601edc0342995eb54b0679256b2d02
LilSeidel <119064389+KevinSoto9@users.noreply.github.com>
2023-05-11 14:04:03 +0200

    first commit

---
+++ TrabajoCalculadoraSubnetic/Logica/Subnet.cs
@@ -0,0 +1,11 @@
+﻿namespace TrabajoCalculadoraSubnetic.Logica
+{
+    public class Subnet
+    {
+        public string Name { get; set; }
+        public int Size { get; set; }
+        public string SubnetMask { get; set; }
+        public string AddressRange { get; set; }
+        public string BroadcastAddress { get; set; }
+    }
+}
